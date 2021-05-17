a27a9075244427de5219cb0834264de958a4ea24
Akash Shyam <shyamiy@gmail.com>
2021-05-17 18:49:27 +0530

    🎨  chore: changed string to template literal

--- src/colorConversion.ts
+++ src/colorConversion.ts
@@ -5,15 +5,15 @@ export function hexToRGB(hex: string) {
 
 	// 3 digits + '#'
 	if (hex.length == 4) {
-		r = '0x' + hex[1] + hex[1];
-		g = '0x' + hex[2] + hex[2];
-		b = '0x' + hex[3] + hex[3];
+		r = `0x${hex[1]}${hex[1]}`;
+		g = `0x${hex[2]}${hex[2]}`;
+		b = `0x${hex[3]}${hex[3]}`;
 
 		// 6 digits + '#'
 	} else if (hex.length == 7) {
-		r = '0x' + hex[1] + hex[2];
-		g = '0x' + hex[3] + hex[4];
-		b = '0x' + hex[5] + hex[6];
+		r = `0x${hex[1]}${hex[2]}`;
+		g = `0x${hex[3]}${hex[4]}`;
+		b = `0x${hex[5]}${hex[6]}`;
 	} else {
 		throw new TypeError('Hex format is incorrect. Please use 3 or 6 hex values.');
 	}
