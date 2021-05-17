7f6392349c9eb04be8c7becd9f1947425b6ee745
Akash Shyam <shyamiy@gmail.com>
2021-05-17 21:30:39 +0530

    ✨  feat(color conversion): rgb to hex

--- src/colorConversion.ts
+++ src/colorConversion.ts
@@ -1,22 +1,38 @@
+export type Color = number | string;
+
 export function hexToRGB(hex: string) {
-	let r: number | string = 0,
-		g: number | string = 0,
-		b: number | string = 0;
+	let r: Color = 0,
+		g: Color = 0,
+		b: Color = 0;
 
 	// 3 digits + '#'
-	if (hex.length == 4) {
+	if (hex.length === 4) {
 		r = `0x${hex[1]}${hex[1]}`;
 		g = `0x${hex[2]}${hex[2]}`;
 		b = `0x${hex[3]}${hex[3]}`;
-
-		// 6 digits + '#'
-	} else if (hex.length == 7) {
+	} else if (hex.length === 7) {
 		r = `0x${hex[1]}${hex[2]}`;
 		g = `0x${hex[3]}${hex[4]}`;
 		b = `0x${hex[5]}${hex[6]}`;
-	} else {
-		throw new TypeError('Hex format is incorrect. Please use 3 or 6 hex values.');
 	}
 
 	return `rgb('${+r}, ${+g}, ${+b}')`;
+}
+
+export function RGBToHex(r: Color, g: Color, b: Color, a?: Color) {
+	r = r.toString(16);
+	g = g.toString(16);
+	b = b.toString(16);
+	a = a?.toString(16);
+
+	if (r.length === 1) r = '0' + r;
+	if (g.length === 1) g = '0' + g;
+	if (b.length === 1) b = '0' + b;
+	if (a?.length === 1) a = '0' + a;
+
+	if (a) {
+		return `#${r}${g}${b}${a}`;
+	}
+
+	return `#${r}${g}${b}`;
 }
--- src/tests/colorConversion.test.ts
+++ src/tests/colorConversion.test.ts
@@ -1,16 +1,24 @@
-import { hexToRGB } from '../colorConversion';
+import { hexToRGB, RGBToHex } from '../colorConversion';
 
 import * as mocha from 'mocha';
 import * as chai from 'chai';
 
 const expect = chai.expect;
 
-describe('Color Conversions', () => {
+describe('Color Conversion', () => {
 	it('Hex To RGB - 6 values', () => {
 		expect(hexToRGB('#ffffff')).to.equal("rgb('255, 255, 255')");
 	});
 
 	it('Hex To RGB - 3 values', () => {
 		expect(hexToRGB('#fff')).to.equal("rgb('255, 255, 255')");
+	});
+
+	it('RGB to Hex', () => {
+		expect(RGBToHex(255, 255, 255)).to.equal('#ffffff');
+	});
+
+	it('RGBA to Hex', () => {
+		expect(RGBToHex(255, 255, 255, 2)).to.equal('#ffffff02');
 	});
 });
